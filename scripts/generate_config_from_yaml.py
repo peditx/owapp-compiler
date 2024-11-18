df73d68d1f67c212c1551cc826f08242130fee38
PeDitX <pedram.ale@me.com>
2024-11-18 22:25:25 +0330

    Update generate_config_from_yaml.py

--- scripts/generate_config_from_yaml.py
+++ scripts/generate_config_from_yaml.py
@@ -1,24 +1,21 @@
 import yaml
 
 def generate_config(yaml_file, config_file):
-    with open(yaml_file, 'r') as file:
-        data = yaml.safe_load(file)
-
+    with open(yaml_file, 'r') as f:
+        data = yaml.safe_load(f)
+    
     with open(config_file, 'w') as f:
         for key, value in data.items():
-            if isinstance(value, dict):  # Check if the value is a dictionary
-                for subkey, subvalue in value.items():
-                    f.write(f"CONFIG_{key}_{subkey.upper()}={subvalue}\n")
+            if isinstance(value, list):
+                # اگر مقدار یک لیست بود، هر آیتم را به صورت جداگانه اضافه کن
+                for item in value:
+                    f.write(f"CONFIG_{key}_{item.upper()}={item}\n")
             else:
+                # در غیر اینصورت به طور مستقیم از value استفاده کن
                 f.write(f"CONFIG_{key}_{value.upper()}={value}\n")
 
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 3:
-        print("Usage: python generate_config_from_yaml.py <yaml_file> <config_file>")
-        sys.exit(1)
-    
-    yaml_file = sys.argv[1]
-    config_file = sys.argv[2]
-    
-    generate_config(yaml_file, config_file)
+# فایل yaml و مقصد config
+yaml_file = 'config.yaml'
+config_file = '.config'
+
+generate_config(yaml_file, config_file)
