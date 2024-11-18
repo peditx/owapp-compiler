430c14ab174ea814a8bbd0c9e1f1e92e9047d539
PeDitX <pedram.ale@me.com>
2024-11-18 22:56:31 +0330

    Update generate_config_from_yaml.py

--- scripts/generate_config_from_yaml.py
+++ scripts/generate_config_from_yaml.py
@@ -14,16 +14,27 @@ def generate_config(yaml_file, config_file):
                 for subkey, subvalue in value.items():
                     if isinstance(subvalue, list):
                         for item in subvalue:
-                            f.write(f"CONFIG_{key}_{subkey}_{item.upper()}={item}\n")
+                            if isinstance(item, str):  # بررسی اینکه item یک رشته باشد
+                                f.write(f"CONFIG_{key}_{subkey}_{item.upper()}={item}\n")
+                            else:
+                                # اگر item یک دیکشنری یا نوع دیگری است، آن را به صورت پیش‌فرض بنویسیم
+                                f.write(f"CONFIG_{key}_{subkey}_{str(item)}={str(item)}\n")
                     else:
-                        f.write(f"CONFIG_{key}_{subkey.upper()}={subvalue}\n")
+                        if isinstance(subvalue, str):
+                            f.write(f"CONFIG_{key}_{subkey.upper()}={subvalue}\n")
+                        else:
+                            f.write(f"CONFIG_{key}_{subkey.upper()}={str(subvalue)}\n")
             elif isinstance(value, list):
                 # اگر مقدار لیست است
                 for item in value:
-                    f.write(f"CONFIG_{key}_{item.upper()}={item}\n")
+                    if isinstance(item, str):  # بررسی اینکه item یک رشته باشد
+                        f.write(f"CONFIG_{key}_{item.upper()}={item}\n")
+                    else:
+                        # اگر item یک دیکشنری یا نوع دیگری است، آن را به صورت پیش‌فرض بنویسیم
+                        f.write(f"CONFIG_{key}_{str(item)}={str(item)}\n")
             else:
                 # اگر مقدار تنها یک مقدار است
-                f.write(f"CONFIG_{key}_{value.upper()}={value}\n")
+                f.write(f"CONFIG_{key}_{str(value).upper()}={str(value)}\n")
 
 if __name__ == "__main__":
     yaml_file = sys.argv[1]
